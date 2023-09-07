502609e7d73cab04c3f569363b61cc5f1c37ae91
“wheslancardoso” <wheslancardoso1@gmail.com>
2023-09-07 16:00:18 -0300

    edit: separating the packages with line breaks

--- archsettingsinstall.sh
+++ archsettingsinstall.sh
@@ -11,13 +11,24 @@ sudo pacman -Sy
 echo -e "\e[1;34m*******************************"
 echo "Installing essential system packages"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm dialog mtools dosfstools avahi acpi acpid gvfs gvfs-smb tar 
+sudo pacman -S --noconfirm dialog 
+sudo pacman -S --noconfirm mtools 
+sudo pacman -S --noconfirm dosfstools 
+sudo pacman -S --noconfirm avahi 
+sudo pacman -S --noconfirm acpi 
+sudo pacman -S --noconfirm acpid 
+sudo pacman -S --noconfirm gvfs 
+sudo pacman -S --noconfirm gvfs-smb 
+sudo pacman -S --noconfirm tar 
 
 # Basic packages
 echo -e "\e[1;34m*******************************"
 echo "Installing basic packages"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm base-devel curl make wget
+sudo pacman -S --noconfirm base-devel 
+sudo pacman -S --noconfirm curl 
+sudo pacman -S --noconfirm make 
+sudo pacman -S --noconfirm wget
 
 echo -e "\e[1;34m*******************************"
 echo "Ordering Mirrors"
@@ -38,7 +49,8 @@ makepkg -si
 echo -e "\e[1;34m*******************************"
 echo "Installing audio packages"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm pulseaudio pavucontrol
+sudo pacman -S --noconfirm pulseaudio 
+sudo pacman -S --noconfirm pavucontrol
 
 # CPU microcode for AMD
 echo -e "\e[1;34m*******************************"
@@ -82,25 +94,41 @@ sudo pacman -S --noconfirm xorg-server xorg-xinit mesa
 echo -e "\e[1;34m*******************************"
 echo "Installing i3 essential packages"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm i3 i3status polybar dmenu picom rofi dunst arandr unzip vim
+sudo pacman -S --noconfirm i3 
+sudo pacman -S --noconfirm i3status 
+sudo pacman -S --noconfirm polybar 
+sudo pacman -S --noconfirm dmenu 
+sudo pacman -S --noconfirm picom 
+sudo pacman -S --noconfirm rofi 
+sudo pacman -S --noconfirm dunst 
+sudo pacman -S --noconfirm arandr 
+sudo pacman -S --noconfirm unzip 
+sudo pacman -S --noconfirm vim
 
 # Appearance
 echo -e "\e[1;34m*******************************"
 echo "Installing lxappearance and qt5ct"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm lxappearance qt5ct
+sudo pacman -S --noconfirm lxappearance 
+sudo pacman -S --noconfirm qt5ct
 
 # Other tools
 echo -e "\e[1;34m*******************************"
 echo "Installing useful tools (lxinput, gparted, copyq, flameshot)"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm lxinput gparted copyq flameshot exa bat
+sudo pacman -S --noconfirm lxinput 
+sudo pacman -S --noconfirm gparted 
+sudo pacman -S --noconfirm copyq 
+sudo pacman -S --noconfirm flameshot 
+sudo pacman -S --noconfirm exa 
+sudo pacman -S --noconfirm bat
 
 # File manager
 echo -e "\e[1;34m*******************************"
 echo "Installing thunar and vifm"
 echo "*******************************\e[0m"
-sudo pacman -S --noconfirm thunar vifm
+sudo pacman -S --noconfirm thunar 
+sudo pacman -S --noconfirm vifm
 
 # Alacritty install
 echo -e "\e[1;34m*******************************"
@@ -112,7 +140,8 @@ sudo pacman -S --noconfirm alacritty
 echo -e "\e[1;34m*******************************"
 echo "Installing neofetch and htop"
 echo -e "*******************************\e[0m"
-sudo pacman -S --noconfirm neofetch htop
+sudo pacman -S --noconfirm neofetch 
+sudo pacman -S --noconfirm btop
 
 echo -e "\e[1;34m*******************************"
 echo "Installing brave"
